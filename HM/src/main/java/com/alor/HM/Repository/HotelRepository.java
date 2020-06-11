24820d2841e9a479647363696b23efa3459b09c5
nehagorakh <nehagorakhpuriya@gmail.com>
2020-06-12 04:41:45 +0530

    completed

--- HM/src/main/java/com/alor/HM/Repository/HotelRepository.java
+++ HM/src/main/java/com/alor/HM/Repository/HotelRepository.java
@@ -11,8 +11,8 @@ import com.alor.HM.Entity.HotelEntity;
 @Repository
 public interface HotelRepository extends CrudRepository<HotelEntity, Integer>{
 
-	List<HotelEntity> findByIdInAndCity(List<Integer> hotelIds, String city);
+	List<HotelEntity> findByIdInAndCityAndIsActive(List<Integer> hotelIds, String city, int isActive);
 
-	List<HotelEntity> findByIdIn(Set<Integer> hotelIds);
+	List<HotelEntity> findByIdInAndIsActive(Set<Integer> hotelIds, int isActive);
 
 }
--- HM/src/main/java/com/alor/HM/ServiceImpl/HotelServiceImpl.java
+++ HM/src/main/java/com/alor/HM/ServiceImpl/HotelServiceImpl.java
@@ -22,6 +22,7 @@ import com.alor.HM.Entity.HotelEntity;
 import com.alor.HM.Entity.HotelFaclity;
 import com.alor.HM.Entity.HotelRoomBooking;
 import com.alor.HM.Entity.RatingEntity;
+import com.alor.HM.Entity.UserEntity;
 import com.alor.HM.Repository.HotelRepository;
 import com.alor.HM.Service.HotelFacilityService;
 import com.alor.HM.Service.HotelRoomBookingService;
@@ -90,18 +91,19 @@ public class HotelServiceImpl implements HotelService{
      * returning all the hotels by given search creterias
      */
 	@Override
-	public List<HotelResponseBean> getHotels(String city, Integer numberOfRooms, String date, Integer wifi, Integer resturant, Integer ac, Integer mealsIncluded,
-			Integer rating) throws ParseException {
-		
+	public List<HotelResponseBean> getHotels(String city, Integer numberOfRooms, String date, Integer wifi,
+			Integer resturant, Integer ac, Integer mealsIncluded, Integer rating) throws ParseException {
+
 		int numberOfRoomsInt = numberOfRooms != null ? numberOfRooms : 0;
-		int ratingInt = rating != null ? rating : 0;;
-		int wifiInt = wifi != null ? wifi : 0;;
-		int resturantInt = resturant != null ? resturant : 0;;
-		int acInt = ac != null ? ac : 0;;
-		int mealsIncludedInt = mealsIncluded != null ? mealsIncluded : 0;;
-		List<HotelEntity> hotelEntities;		
+		int ratingInt = rating != null ? rating : 0;
+		int wifiInt = wifi != null ? wifi : 0;
+		int resturantInt = resturant != null ? resturant : 0;
+		int acInt = ac != null ? ac : 0;
+		int mealsIncludedInt = mealsIncluded != null ? mealsIncluded : 0;
+
+		List<HotelEntity> hotelEntities;
 		List<HotelResponseBean> hotelResponseBeanList = new ArrayList<HotelResponseBean>();
-		
+
 		// first getting hotelIds for given facilities
 		List<HotelFaclity> hotelFacilityList = hotelFacilityService.getHotelFacilityByAllGivenFacilities(wifiInt,
 				resturantInt, acInt, mealsIncludedInt);
@@ -109,8 +111,8 @@ public class HotelServiceImpl implements HotelService{
 		Set<Integer> hotelIds = hotelFacilityList.stream().map(hf -> hf.getHotelId()).collect(Collectors.toSet());
 
 		// getting hotel id's with given city
-		if (city != null && city.equalsIgnoreCase("")) {
-			hotelEntities = hotelRepository.findByIdInAndCity((List) hotelIds, city);
+		if (city != null && !city.equalsIgnoreCase("")) {
+			hotelEntities = hotelRepository.findByIdInAndCityAndIsActive(new ArrayList<Integer>(hotelIds), city, 1);
 			hotelIds = hotelEntities.stream().map(hf -> hf.getId()).collect(Collectors.toSet());
 		}
 		// if date is null : generating current Date
@@ -123,24 +125,19 @@ public class HotelServiceImpl implements HotelService{
 		Map<Integer, List<RatingEntity>> hotelRatingMapGroupByRating = hotelRatings.stream()
 				.collect(Collectors.groupingBy(RatingEntity::getHotelId));
 
-		Map<Integer, List<HotelEntity>> hotelMap = hotelRepository.findByIdIn(hotelIds).stream()
-				.collect(Collectors.groupingBy(HotelEntity::getId));
-
 		Map<Integer, List<HotelFaclity>> hotelFacilityMap = hotelFacilityList.stream()
 				.collect(Collectors.groupingBy(HotelFaclity::getHotelId));
 
-		// getting list of hotels where some rooms are booked on given date or today
 		List<HotelRoomBooking> hotelRoomBookings = hotelRoomBookingService.getHotelRoomBookingRecords(hotelIds, date);
-		List<HotelResponseBean> hotelResponseBeanListWithSomeBookedRooms = getHotelWithSomeBookedAndAvailableRooms(
-				hotelRoomBookings, hotelMap, hotelFacilityMap, hotelRatingMapGroupByRating);
 
-		// for hotels which is not having any booking for given date ot today
-		hotelIds.removeAll(hotelRoomBookings.stream().map(hrb -> hrb.getHotelId()).collect(Collectors.toList()));
-		List<HotelResponseBean> hotelResponseBeanListWithAllAvailableRooms = getHotelWithAllAvailbaleRooms(hotelIds,
-				hotelMap, hotelFacilityMap, hotelRatingMapGroupByRating);
+		Map<Integer, List<HotelRoomBooking>> hotelRoomBookingMapListGroupByHotelId = (hotelRoomBookings.stream()
+				.collect(Collectors.groupingBy(HotelRoomBooking::getHotelId)));
 
-		hotelResponseBeanList.addAll(hotelResponseBeanListWithSomeBookedRooms);
-		hotelResponseBeanList.addAll(hotelResponseBeanListWithAllAvailableRooms);
+		// get all the hotels with given filters
+		List<HotelEntity> hotelEntityList = hotelRepository.findByIdInAndIsActive(hotelIds, 1);
+
+		hotelResponseBeanList = getHotelWithAllAvailbaleRooms(hotelEntityList, hotelFacilityMap,
+				hotelRoomBookingMapListGroupByHotelId, hotelRatingMapGroupByRating);
 
 		if (ratingInt != 0)
 			hotelResponseBeanList = hotelResponseBeanList.stream().filter(hrb -> hrb.getRating() == rating)
@@ -150,64 +147,40 @@ public class HotelServiceImpl implements HotelService{
 			hotelResponseBeanList = hotelResponseBeanList.stream()
 					.filter(hrb -> hrb.getAvailableRooms() >= numberOfRoomsInt).collect(Collectors.toList());
 
-		return hotelResponseBeanList.stream().sorted().collect(Collectors.toList());
+		return hotelResponseBeanList.stream().sorted(Comparator.comparingInt(HotelResponseBean::getRating))
+				.collect(Collectors.toList());
 	}
-	
-	
-	private List<HotelResponseBean> getHotelWithAllAvailbaleRooms(Set<Integer> hotelIds,
-			Map<Integer, List<HotelEntity>> hotelMap, Map<Integer, List<HotelFaclity>> hotelFacilityMap,
-			Map<Integer, List<RatingEntity>> hotelRatingMapGroupByRating) {
+	/**
+	 * 
+	 * @param hotelEntityList
+	 * @param hotelFacilityMap
+	 * @param hotelRoomBookingMapListGroupByHotelId
+	 * @param hotelRatingMapGroupByRating
+	 * @return
+	 * setting hotel properties and average rating and available rooms.
+	 */
+		
+	private List<HotelResponseBean> getHotelWithAllAvailbaleRooms(List<HotelEntity> hotelEntityList, Map<Integer, 
+			List<HotelFaclity>> hotelFacilityMap, Map<Integer, List<HotelRoomBooking>> hotelRoomBookingMapListGroupByHotelId, Map<Integer, List<RatingEntity>> hotelRatingMapGroupByRating) {
 
 		List<HotelResponseBean> hotelResponseBeanList = new ArrayList<HotelResponseBean>();
-		for (int hoteId : hotelIds) {
+		for (HotelEntity hotelEntity : hotelEntityList) {
 
-			HotelResponseBean hotelResponseBean1 = new HotelResponseBean();
-			hotelResponseBean1.setHotelId(hoteId);
-			BeanUtils.copyProperties(hotelMap.get(hoteId).get(0), hotelResponseBean1);
-			BeanUtils.copyProperties(hotelFacilityMap.get(hoteId), hotelResponseBean1);
-
-			hotelResponseBean1.setAvailableRooms(hotelMap.get(hoteId).get(0).getNumberOfRooms());
+			HotelResponseBean hotelResponseBean = new HotelResponseBean();
+			BeanUtils.copyProperties(hotelEntity, hotelResponseBean);
+			BeanUtils.copyProperties(hotelFacilityMap.get(hotelEntity.getId()).get(0), hotelResponseBean);
+            
+			// getting and setting available rooms
+			if(hotelRoomBookingMapListGroupByHotelId.get(hotelEntity.getId()) != null)
+				hotelResponseBean.setAvailableRooms(hotelResponseBean.getNumberOfRooms()						- (hotelRoomBookingMapListGroupByHotelId.get(hotelEntity.getId()).stream().mapToInt(hrb -> hrb.getNumberOfRooms()).sum()));
+					
+			else
+				hotelResponseBean.setAvailableRooms(hotelEntity.getNumberOfRooms());
 
 			if (hotelRatingMapGroupByRating.size() > 0)
-				hotelResponseBean1.setAverageRating((int) hotelRatingMapGroupByRating.get(hoteId).stream()
+				hotelResponseBean.setAverageRating((int) hotelRatingMapGroupByRating.get(hotelEntity.getId()).stream()
 						.mapToDouble(r -> r.getRating()).average().getAsDouble());
-			hotelResponseBeanList.add(hotelResponseBean1);
-		}
-		return hotelResponseBeanList;
-	}
-	
-	private List<HotelResponseBean> getHotelWithSomeBookedAndAvailableRooms(List<HotelRoomBooking> hotelRoomBookings,
-			Map<Integer, List<HotelEntity>> hotelMap, Map<Integer, List<HotelFaclity>> hotelFacilityMap,
-			Map<Integer, List<RatingEntity>> hotelRatingMapGroupByRating) {
-
-		List<HotelResponseBean> hotelResponseBeanList = new ArrayList<HotelResponseBean>();
-		HotelResponseBean hotelResponseBean;
-
-		if (hotelRoomBookings.size() > 0) {
-
-			Map<Integer, List<HotelRoomBooking>> hotelRoomBookingMapListGroupByHotelId = (hotelRoomBookings.stream()
-					.collect(Collectors.groupingBy(HotelRoomBooking::getHotelId)));
-
-			// for hotels which is having booking on given date or today
-			for (Map.Entry<Integer, List<HotelRoomBooking>> entry : hotelRoomBookingMapListGroupByHotelId.entrySet()) {
-
-				hotelResponseBean = new HotelResponseBean();
-				hotelResponseBean.setHotelId(entry.getKey());
-
-				BeanUtils.copyProperties(hotelMap.get(hotelResponseBean.getHotelId()).get(0), hotelResponseBean);
-				BeanUtils.copyProperties(hotelFacilityMap.get(hotelResponseBean.getHotelId()).get(0),
-						hotelResponseBean);
-
-				// getting and setting available tooms
-				hotelResponseBean.setAvailableRooms(hotelResponseBean.getNumberOfRooms()
-						- (entry.getValue().stream().mapToInt(hrb -> hrb.getNumberOfRooms()).sum()));
-
-				// getting and setting rating
-				if (hotelRatingMapGroupByRating.size() > 0)
-					hotelResponseBean.setAverageRating((int) hotelRatingMapGroupByRating.get(entry.getKey()).stream()
-							.mapToDouble(r -> r.getRating()).average().getAsDouble());
-				hotelResponseBeanList.add(hotelResponseBean);
-			}
+			hotelResponseBeanList.add(hotelResponseBean);
 		}
 		return hotelResponseBeanList;
 	}
